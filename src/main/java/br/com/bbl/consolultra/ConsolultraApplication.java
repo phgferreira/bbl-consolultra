aecebe9dad5e2df61917cc093492bde25f12ad64
Paulo Ferreira <phgferreira@outlook.com>
2021-04-16 09:58:02 -0300

    Start Spring Boot

---
+++ src/main/java/br/com/bbl/consolultra/ConsolultraApplication.java
@@ -0,0 +1,13 @@
+package br.com.bbl.consolultra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsolultraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsolultraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bbl/consolultra/ConsolultraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bbl.consolultra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsolultraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
